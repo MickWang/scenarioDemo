e27b10d63c0af4c3b1ff53377b51669fe4d4bc4f
Mickey Wang <1244134672@qq.com>
2016-08-19 16:28:47 +0800

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scenarioDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scenarioDemoApp
+ */
+angular.module('scenarioDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scenarioDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scenarioDemoApp
+ */
+angular.module('scenarioDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
